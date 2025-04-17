f31e8876be24b9539baacb34cfda0ac9b74257c3
Jivebread <spark@troy.edu>
2025-04-17 17:28:14 +0900

    chore: jpa 설정값과 test container 설정값을 지정합니다. - jpa config 값과 application.yml, test 설정합니다.

--- src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
+++ src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
@@ -1,19 +1,43 @@
 package kr.hhplus.be.server.config.jpa;
 
+import jakarta.persistence.EntityManagerFactory;
+import java.util.Arrays;
+import javax.sql.DataSource;
+import kr.hhplus.be.server.concerts.domain.ConcertScheduleRepository;
+import kr.hhplus.be.server.concerts.infra.DefaultConcertRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@EnableJpaAuditing
-@EnableJpaRepositories
+@EnableJpaRepositories(
+    basePackages = "kr.hhplus.be.server",
+    entityManagerFactoryRef = "customEntityManagerFactory"
+)
 public class JpaConfig {
+    @Bean("customEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(final DataSource datasource){
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
 
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new JpaTransactionManager();
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("kr.hhplus.be.server");
+        factory.setDataSource(datasource);
+        return factory;
     }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
 }
--- src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/be/server/TestcontainersConfiguration.java
@@ -1,28 +1,53 @@
 package kr.hhplus.be.server;
 
 import jakarta.annotation.PreDestroy;
+import java.util.Arrays;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
-@Configuration
+@TestConfiguration
 class TestcontainersConfiguration {
 
   public static final MySQLContainer<?> MYSQL_CONTAINER;
-
+  public static Network sharedNetwork = Network.newNetwork();
   static {
     MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
         .withDatabaseName("hhplus")
         .withUsername("test")
-        .withPassword("test");
-    MYSQL_CONTAINER.start();
+        .withPassword("test")
+        .withExposedPorts(3306)
+        .withNetwork(sharedNetwork)
+        .withNetworkAliases("mysql")
+        .waitingFor(Wait.forListeningPort());
 
+    MYSQL_CONTAINER.start();
+    String host = MYSQL_CONTAINER.getHost();
+    int mappedPort = MYSQL_CONTAINER.getMappedPort(3306);
     System.setProperty("spring.datasource.url",
-        MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
+        "jdbc:mysql://" + host + ":" + mappedPort + "/hhplus" + "?characterEncoding=UTF-8&serverTimezone=UTC");
     System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
     System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
   }
+  @DynamicPropertySource
+  static void dynamicProperties(ApplicationContext applicationContext) {
+    MYSQL_CONTAINER.start();
 
+    System.setProperty("spring.datasource.url", "jdbc:mysql://" + MYSQL_CONTAINER.getHost() + ":" + MYSQL_CONTAINER.getMappedPort(3306) + "/hhplus?characterEncoding=UTF-8&serverTimezone=UTC");
+    System.setProperty("spring.datasource.username", "test");
+    System.setProperty("spring.datasource.password", "test");
+
+    System.setProperty("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+    System.setProperty("spring.jpa.properties.hibernate.timezone.default_storage", "NORMALIZE_UTC");
+    System.setProperty("spring.jpa.properties.hibernate.jdbc.time_zone", "UTC");
+
+    System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
+  }
   @PreDestroy
   public void preDestroy() {
     if (MYSQL_CONTAINER.isRunning()) {
