7e99ec687e1365f64c5c6b103e071f59845c566a
Jivebread <spark@troy.edu>
2025-05-17 11:29:26 +0900

    feature(Token): 기존 jpa Repository를 삭제합니다. - redis로 token 값을 이전을 위한 처리를 진행합니다. - Token 값을 영속화 하지 않는 방향으로 처리합니다.

--- src/main/java/kr/hhplus/be/server/queues/infra/RedisTokenRepository.java
+++ src/main/java/kr/hhplus/be/server/queues/infra/RedisTokenRepository.java
@@ -5,9 +5,20 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import kr.hhplus.be.server.queues.domain.Token;
+import kr.hhplus.be.server.queues.domain.TokenRepository;
 
-public class RedisTokenRepository implements DefaultTokenRepository{
+public class RedisTokenRepository implements TokenRepository {
 
+
+  @Override
+  public Token save(Token token) {
+    return null;
+  }
+
+  @Override
+  public Optional<Token> findById(UUID tokenId) {
+    return Optional.empty();
+  }
 
   @Override
   public Optional<Token> findByUserId(UUID userId) {
