cf0a9b24f612d8862e1545156f93efe449368eae
Jivebread <spark@troy.edu>
2025-05-17 11:29:43 +0900

    feature(Token): 기존 jpa Repository를 삭제합니다. - redis로 token 값을 이전을 위한 처리를 진행합니다. - Token 값을 영속화 하지 않는 방향으로 처리합니다.

--- src/main/java/kr/hhplus/be/server/queues/infra/RedisTokenRepository.java
+++ src/main/java/kr/hhplus/be/server/queues/infra/RedisTokenRepository.java
@@ -1,7 +1,5 @@
 package kr.hhplus.be.server.queues.infra;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import kr.hhplus.be.server.queues.domain.Token;
