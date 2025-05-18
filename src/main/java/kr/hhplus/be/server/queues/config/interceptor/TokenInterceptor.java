5b48363d4e2b464c946b2e22b659cf3a2a4e8312
Jivebread <spark@troy.edu>
2025-05-18 21:16:00 +0900

    feature(Interceptor): 기존 token 검증을 db 기반에서 nosql 으로 이전합니다.
    - 사용하지 않는 메서드는 삭제합니다.

--- src/main/java/kr/hhplus/be/server/queues/config/interceptor/TokenInterceptor.java
+++ src/main/java/kr/hhplus/be/server/queues/config/interceptor/TokenInterceptor.java
@@ -1,8 +1,11 @@
-package kr.hhplus.be.server.queues.application;
+package kr.hhplus.be.server.queues.config.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.UUID;
+import kr.hhplus.be.server.queues.application.QueueAccessVerifier;
+import kr.hhplus.be.server.queues.application.QueueFactoryUseCase;
+import kr.hhplus.be.server.queues.application.TokenFactoryUseCase;
 import kr.hhplus.be.server.queues.domain.QueueStatus;
 import kr.hhplus.be.server.queues.domain.Token;
 import org.springframework.stereotype.Component;
@@ -10,13 +13,10 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
 
-  private final QueueFactoryUseCase queueFactoryUseCase;
-  private final TokenFactoryUseCase tokenFactoryUseCase;
+  private final QueueAccessVerifier queueAccessVerifier;
 
-  public TokenInterceptor(QueueFactoryUseCase queueFactoryUseCase,
-      TokenFactoryUseCase tokenFactoryUseCase) {
-    this.queueFactoryUseCase = queueFactoryUseCase;
-    this.tokenFactoryUseCase = tokenFactoryUseCase;
+  public TokenInterceptor(QueueAccessVerifier queueAccessVerifier) {
+    this.queueAccessVerifier = queueAccessVerifier;
   }
 
   @Override
@@ -26,8 +26,10 @@ public class TokenInterceptor implements HandlerInterceptor {
     if (tokenId == null || tokenId.isBlank()) {
       throw new RuntimeException("토큰이 존재하지 않습니다.");
     }
-    Token token = tokenFactoryUseCase.getToken(UUID.fromString(tokenId));
-    QueueStatus queueStatus = queueFactoryUseCase.getQueue(token.getUserId()).getQueueStatus();
-    return queueStatus == QueueStatus.PROCESSING;
+    if (queueAccessVerifier.canAccess(tokenId)){
+      return true;
+    }
+
+    return false;
   }
 }
