c3756c5c83b852727938ff4a05fce27c589be0f3
jivebreaddev <jivebreaddev@gmail.com>
2025-05-22 21:43:52 +0900

    test(Queue): Queue를 검증하기 위한 이벤트들에 대한 테스트를 작성합니다.

---
+++ src/test/java/kr/hhplus/be/server/queues/application/QueueAccessVerifierTest.java
@@ -0,0 +1,57 @@
+package kr.hhplus.be.server.queues.application;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+import kr.hhplus.be.server.queues.domain.Queue;
+import kr.hhplus.be.server.queues.domain.QueueRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class QueueAccessVerifierTest {
+  @Mock
+  private QueueRepository queueRepository;
+  @Mock
+  private QueueAccessVerifier queueAccessVerifier;
+
+  @Test
+  void canAccess_whenTokenExists_shouldReturnTrue() {
+    // given
+    UUID userId = UUID.randomUUID();
+    Queue mockQueue = Queue.of(userId);
+    when(queueRepository.findByUserId(userId)).thenReturn(Optional.of(mockQueue));
+
+    // when
+    boolean result = queueAccessVerifier.canAccess(userId.toString());
+
+    // then
+    assertTrue(result);
+    verify(queueRepository, times(1)).findByUserId(userId);
+  }
+
+  @Test
+  @DisplayName("token 없으면 오류를 표기합니다.")
+  void withoutTokenNotAllowed() {
+    UUID userId = UUID.randomUUID();
+    Queue queue = mock(Queue.class);
+    when(queue.isProcessing()).thenReturn(false);
+    when(queueRepository.findByUserId(userId)).thenReturn(Optional.of(queue));
+
+    // when
+    boolean result = queueAccessVerifier.canAccess(userId.toString());
+
+    // then
+    assertFalse(result);
+    verify(queueRepository).findByUserId(userId);
+  }
+}
