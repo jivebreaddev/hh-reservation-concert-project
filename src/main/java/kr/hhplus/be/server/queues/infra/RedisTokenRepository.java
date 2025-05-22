6208c86333cbdb117dbefe3800fe705d19c0dd41
jivebreaddev <jivebreaddev@gmail.com>
2025-05-22 12:57:48 +0900

    no message

--- src/main/java/kr/hhplus/be/server/queues/infra/RedisTokenRepository.java
+++ src/main/java/kr/hhplus/be/server/queues/infra/RedisTokenRepository.java
@@ -1,21 +1,12 @@
 package kr.hhplus.be.server.queues.infra;
 
+
+import kr.hhplus.be.server.queues.domain.Token;
+
 import java.util.Optional;
 import java.util.UUID;
-import kr.hhplus.be.server.queues.domain.Token;
-import kr.hhplus.be.server.queues.domain.TokenRepository;
 
-public class RedisTokenRepository implements TokenRepository {
-
-  @Override
-  public Token save(Token token) {
-    return null;
-  }
-
-  @Override
-  public Optional<Token> findById(UUID tokenId) {
-    return Optional.empty();
-  }
+public class RedisTokenRepository implements DefaultTokenRepository{
 
   @Override
   public Optional<Token> findByUserId(UUID userId) {
